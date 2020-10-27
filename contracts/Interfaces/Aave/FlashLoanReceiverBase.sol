2900acdead4444b5d5b276112333a5c22238b7d2
Sam Priestley <sfpriestley@hotmail.co.uk>
2020-10-27 14:31:57 +0000

    normalised openzeppelin imports

--- contracts/Interfaces/Aave/FlashLoanReceiverBase.sol
+++ contracts/Interfaces/Aave/FlashLoanReceiverBase.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.6.6;
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelinV3/contracts/math/SafeMath.sol";
+import "@openzeppelinV3/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelinV3/contracts/token/ERC20/SafeERC20.sol";
 import "./IFlashLoanReceiver.sol";
 import "./ILendingPoolAddressesProvider.sol";
 import "../utils/Withdrawable.sol";
